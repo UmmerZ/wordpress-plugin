52f8b4051ce881eaf4810782383fb928482efb30
UmmerZ <ummer.zaman88@gmail.com>
2020-10-07 14:37:25 -0600

    intial commit

---
+++ ummers-calc.php
@@ -0,0 +1,16 @@
+
+<?php
+/**
+ * Plugin Name:       My Basics Plugin
+ * Plugin URI:        https://ummerdar.com/calc-plugin
+ * Description:       simple calculator plugin
+ * Version:           1.0
+ * Requires at least: 5
+ * Requires PHP:      7.2
+ * Author:            Ummer Zaman
+ * Author URI:        https://ummerdar.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
